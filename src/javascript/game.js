d3574a7113e9bd658fffcd1d9ca35b398066a5bc
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2024-07-29 15:21:56 -0500

    Feat: Super-Mario-Phaser. Animación de Saltar y Físicas - Se agrega el Salto. - Con física cae al suelo. - Con colisión se frena al tocar el piso. #20240729c

--- src/javascript/game.js
+++ src/javascript/game.js
@@ -5,6 +5,13 @@ const config = {
   height: 244,
   backgroundColor: '#009dd8', // 'rgb(0, 157, 216)',
   parent: 'game',
+  physics: {
+    default: 'arcade',  // box2d, chipmunk, ninja, p2
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
   scene: {
     preload, // Fn se ejecuta para precargar recursos
     create, // Fn se ejecuta cuando el juego comienza
@@ -35,11 +42,31 @@ function create() {
   this.add.image(100, 50, 'cloud1')
     .setOrigin(0, 0)
     .setScale(0.15);
-  this.mario = this.add.sprite(50, 200, 'mario')
+
+  // this.add.tileSprite(0, config.height - 32, 
+  // config.width, 32,
+  //   'floorbricks')
+  //   .setOrigin(0, 0);
+
+  this.floor = this.physics.add.staticGroup();
+
+  this.floor
+    .create(0, config.height - 32, 'floorbricks')
+    .setOrigin(0, 0)
+    .refreshBody();
+
+  this.floor
+    .create(160, config.height - 32, 'floorbricks')
+    .setOrigin(0, 0)
+    .refreshBody();
+
+  // this.mario = this.add.sprite(50, 200, 'mario')
+  //   .setOrigin(0, 0);
+  this.mario = this.physics.add
+    .sprite(50, config.height - 16 * 4, 'mario')
     .setOrigin(0, 0);
-  this.add.tileSprite(0, config.height - 32, config.width, 32,
-    'floorbricks')
-    .setOrigin(0, 0);
+
+  this.physics.add.collider(this.mario, this.floor);
 
   this.keys = this.input.keyboard.createCursorKeys();
 
@@ -54,22 +81,32 @@ function create() {
   });
   this.anims.create({
     key: 'mario-idle',
-    frames:[{ key: 'mario', frame: 0}]
+    frames: [{ key: 'mario', frame: 0 }]
+  });
+  this.anims.create({
+    key: 'mario-jump',
+    frames: [{ key: 'mario', frame: 5 }]
   });
 }
 
 function update() {
   if (this.keys.left.isDown) {
     // Movemos a mario en el Eje `x` a menos 2 ⬅️ 
-    this.mario.anims.play('mario-walk', true);
     this.mario.x -= 2;
+    this.mario.anims.play('mario-walk', true);
     this.mario.flipX = true;
   } else if (this.keys.right.isDown) {
     // Movemos a mario en el Eje `x` mas 2  ➡️
-    this.mario.anims.play('mario-walk', true);
     this.mario.x += 2;
+    this.mario.anims.play('mario-walk', true);
     this.mario.flipX = false;
   } else {
     this.mario.anims.play('mario-idle', true);
+  }
+
+  if (this.keys.up.isDown) {
+    // Movemos a mario en el Eje `x` a menos 5  ⬆️
+    this.mario.y -= 5;
+    this.mario.anims.play('mario-jump', true);
   }
 }
