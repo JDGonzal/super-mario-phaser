25fa808b14a64cdcaec2a3dae77c48f3b7ce5de0
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2024-08-05 18:39:43 -0500

    Feat: Super-Mario-Phaser. Agregamos Monedas - Lo que hay en el preload de "game.js" , lo pasamos a "spreedsheet.js". - Añadimos monedas - Añadimos un tipo nuevo de colisión llamado "overlap".
    #20240805b

---
+++ src/javascript/spritesheet.js
@@ -0,0 +1,27 @@
+const INIT_SPRITSHEETS = [
+  {
+    key: 'mario',
+    path: './assets/entities/mario.png',
+    frameWidth: 18,
+    frameHeight: 16
+  },
+  {
+    key: 'goomba',
+    path: './assets/entities/overworld/goomba.png',
+    frameWidth: 16,
+    frameHeight: 16
+  },
+  {
+    key: 'coin',
+    path: './assets/collectibles/coin.png',
+    frameWidth: 16,
+    frameHeight: 16
+  }
+]
+
+export const initSpritesheet = ({ load }) => {
+  INIT_SPRITSHEETS.forEach(
+    ({ key, path, frameWidth, frameHeight }) => {
+      load.spritesheet(key, path, { frameWidth, frameHeight });
+    });
+}
